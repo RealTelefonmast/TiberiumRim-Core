dc5cdab0004f7b1fc9c9e3ab198424ed07572c01
Maxim <MaximSw@web.de>
2024-01-09 16:17:13 +0100

    hangar setup

---
+++ Source/TRC/TiberiumRimCore/Data/ThingClasses/Building_Hangar.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+
+namespace TR.ThingClasses;
+
+public class Building_Hangar : TRBuildingPrototype
+{
+    private MechConstructionStack _stack;
+    
+    public override void SpawnSetup(Map map, bool respawningAfterLoad)
+    {
+        base.SpawnSetup(map, respawningAfterLoad);
+        _stack = new MechConstructionStack();
+    }
+}
